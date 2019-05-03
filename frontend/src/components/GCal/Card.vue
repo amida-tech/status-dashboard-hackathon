bee1d9973d4888737cde48e2ca85e40d2464fe9e
steven <steven.staley@amida.com>
2019-05-03 13:25:52 -0400

    gcal-component templates

---
+++ frontend/src/components/GCal/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Card">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
