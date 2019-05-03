9c85f381c13799cf8c16bb35d6d9393cf01831b9
Eric West <eric.west@amida.com>
2019-05-03 12:09:19 -0400

    added basic styling to the component

--- frontend/src/components/github-commit-thing/GitHubCommitDisplay.vue
+++ frontend/src/components/github-commit-thing/GitHubCommitDisplay.vue
@@ -1,34 +1,46 @@
 <template>
   <div>
-    <br/>
+    <br>
     <div>GitHubCommitDisplay</div>
     <div>{{ hmm }}</div>
-    <br/>
+    <br>
+    <br>
+    <div class="github-feed">
+        <div class="github-feed__feed-header">
+          Most Recently Updated Repositories:
+        </div>
+        <div class="github-feed__feed-items">
+          <div class="github-feed__feed-items-project">PROJECT 1 </div>
+          <div class="github-feed__feed-items-project">PROJECT 2 </div>
+          <div class="github-feed__feed-items-project">PROJECT 3 </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState } from "vuex";
 
 export default {
-  name: 'GitHubCommitDisplay',
+  name: "GitHubCommitDisplay",
   props: {
-    msg: String,
+    msg: String
   },
   computed: {
-    ...mapState(['wmata']),
+    ...mapState(["wmata"])
   },
   async mounted() {
     // this.fetchWmata();
     // TODO: GitHub launch fetch
   },
   methods: {
-    ...mapActions(['fetchWmata']),
-  },
+    ...mapActions(["fetchWmata"])
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
 
+<style scoped lang="scss">
+// @import "..\styles\core.scss"
 </style>
