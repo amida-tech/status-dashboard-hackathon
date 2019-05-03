dd196b4bc5dfc711c1598b236e996b3cc0a141a1
Jon Berry <jon@jonberry.co>
2019-05-03 11:43:17 -0400

    more scaffolding
    Signed-off-by: Jon Berry <jon@jonberry.co>

--- frontend/src/components/transit/TransitView.vue
+++ frontend/src/components/transit/TransitView.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="transit-view">
     <aside class="transit-view__rideshare-section">
-      <section class="transit-view__rideshare-service"></section>
+      <section class="transit-view__rideshare-service">
+        <h2>Uber</h2>
+        <label>Estiamted arrival time</label>
+        <span>4minutes</span>
+        <label>surge multiplier</label>
+        <span>1.2x</span>
+      </section>
       <section class="transit-view__rideshare-service"></section>
       <section class="transit-view__rideshare-service"></section>
       <section class="transit-view__rideshare-service"></section>
       <section class="transit-view__rideshare-service"></section>
     </aside>
     <main class="transit-view__metro-section">
-      <section class="transit-view__metro-station"></section>
+      <section class="transit-view__metro-station">
+        <h2>Dupont Circle</h2>
+        <ul>
+          <label>Service Alerts</label>
+          <li>Due to fire dept activity at Mississippi Ave & 21st St SE, buses may experience delays in both directions.</li>
+        </ul>
+      </section>
       <section class="transit-view__metro-station"></section>
       <section class="transit-view__metro-station"></section>
     </main>
@@ -28,22 +40,42 @@ export default {
     display: flex;
     flex-direction: row;
     border: 1px solid #fbad35;
+    background-color: $lightest-gray;
     height: 1080px;
     width: 1920px;
     justify-content: stretch;
     &__rideshare-section {
-      background-color: #FF0000;
       flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 0.5rem 0.25rem 0.5rem 0.5rem;
     }
     &__rideshare-service {
-      background-color: #0000FF;
+      @extend .material-box-shadow--level-2;
+      background-color: $solitude;
       padding: 0.5rem;
       margin: 0.5rem;
-      height: 216px;
+      flex: 1;
+      border-radius: 0.75rem;
+      display: flex;
+      flex-direction: column;
     }
     &__metro-section {
-      background-color: #00FF00;
+      flex: 1;
+      display: flex;
+      flex-direction: row;
+      padding: 0.5rem 0.5rem 0.5rem 0.25rem;
       flex: 1.618 * 3;
+    }
+    &__metro-station {
+      @extend .material-box-shadow--level-2;
+      background-color: $solitude;
+      padding: 0.5rem;
+      margin: 0.5rem;
+      border-radius: 0.75rem;
+      flex: 1;
+      display: flex;
+      flex-direction: column;
     }
   }
 }
