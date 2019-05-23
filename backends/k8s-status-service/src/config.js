fa68b3d60b332a89734e793304413cceb474ef96
Aaron Houlihan <aaron@amida.com>
2019-05-23 16:07:32 -0400

    k8s-status-service: Disable dockerhub integration

--- backends/k8s-status-service/src/config.js
+++ backends/k8s-status-service/src/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 module.exports = {
-  port: process.env.K8S_STATUS_SERVICE_PORT,
-  dockerHubUsername: process.env.K8S_STATUS_SERVICE_DOCKER_HUB_USERNAME,
-  dockerHubPassword: process.env.K8S_STATUS_SERVICE_DOCKER_HUB_PASSWORD
+  port: process.env.K8S_STATUS_SERVICE_PORT
+  // Docker hub integration is commented out in src/app.js for now.
+  // dockerHubUsername: process.env.K8S_STATUS_SERVICE_DOCKER_HUB_USERNAME,
+  // dockerHubPassword: process.env.K8S_STATUS_SERVICE_DOCKER_HUB_PASSWORD
 }
