c591c4f84d16f15e2ccb612b8802fa0515b23aee
Harry Rickards <harry@amida.com>
2019-05-24 15:42:46 +0000

    Stop running everything as privileged user

--- install.sh
+++ install.sh
@@ -1,18 +1,65 @@
 #!/usr/bin/env bash
+# initial sanity checks
 if [ ! -f /etc/lsb-release ]; then
 	echo "/etc/lsb-release not found"
-	echo "This script must be run on Ubuntu MATE (or another Debian-based system with LightDM as the default login manager). Quitting..."
+	echo "This script must be run on Ubuntu or another Debian-based system. Quitting..."
 	exit 1
 fi
-if [ ! -d /etc/lightdm ]; then
-	echo "/etc/lightdm not found"
-	echo "This script must be run on Ubuntu MATE (or another Debian-based system with LightDM as the default login manager). Quitting..."
+if [[ $EUID -eq 0 ]]; then
+	echo "This script should not be run as root. Quitting... Run again as your local user."
 	exit 1
 fi
 
+# prompt user whether to do kiosk setup, or just deploy code as systemd units
+read -p "Deploy kiosk-mode dashboard GUI? This requires LightDM (y/n) " answer
+KIOSK_MODE=false
+case ${answer:0:1} in
+	y|Y )
+		KIOSK_MODE=true
+		if [ ! -d /etc/lightdm ]; then
+			echo "/etc/lightdm not found"
+			echo "This script must be run on Ubuntu MATE (or another Debian-based system with LightDM as the default login manager). Quitting..."
+			exit 1
+		fi
+	;;
+esac
+
 export BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
-export USER="$(whoami)"
 pushd $BASEDIR
+
+export USER=dashboard
+export PRIVILEGED_USER=$(whoami)
+export DOCKER_USER=$PRIVILEGED_USER
+export HOMEDIR=/home/$USER
+if id $USER >/dev/null 2>&1; then
+	echo "$USER already exists; skipping creation"
+else
+	echo "Creating non-privileged user"
+	# do *not* export this, else it may inadvertently be passed to envsubst
+	PASSWORD="$(pwqgen random=64)"
+	sudo adduser $USER --gecos "Dashboard,,," --disabled-password
+	echo "$USER:$PASSWORD" | sudo chpasswd
+	echo "Write down the following user details in a safe place (e.g., 1Password). You will not see them again."
+	echo "    Username: $USER"
+	echo "    Password: $PASSWORD"
+	read -p "Hit enter when done"
+fi
+
+read -p "Change $PRIVILEGED_USER user password (y/n)? " answer
+case ${answer:0:1} in
+	y|Y )
+		# do *not* export this, else it may inadvertently be passed to envsubst
+		PASSWORD="$(pwqgen random=64)"
+		echo "$PRIVILEGED_USER:$PASSWORD" | sudo chpasswd
+		echo "Write down the following privileged user details in a safe place (e.g., 1Password). You will not see them again."
+		echo "    Username: $PRIVILEGED_USER"
+		echo "    Password: $PASSWORD"
+		read -p "Hit enter when done"
+	;;
+	* )
+		echo "Skipping password change"
+	;;
+esac
 
 echo "Copy the kubecfg file from ~/.kube/config on your machine to $BASEDIR/kubecfg"
 read -p "Hit enter when done"
@@ -26,56 +73,53 @@ export UBER_KEY
 export DARKSKY_KEY
 export GCAL_CREDENTIALS
 
-echo "Installing dependencies"
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs python3-venv python3-dev
-
 echo "Stopping existing services"
-systemctl --user stop amida-dashboard-frontend
-systemctl --user stop amida-dashboard-transit
-systemctl --user stop amida-dashboard-calendar
-systemctl --user stop amida-dashboard-k8s
+sudo systemctl stop amida-dashboard-frontend
+sudo systemctl stop amida-dashboard-transit
+sudo systemctl stop amida-dashboard-calendar
+sudo systemctl stop amida-dashboard-k8s
 
 echo "Setting up frontend"
 envsubst < frontend/src/config.envsubst.js > frontend/src/config.js
-frontend/setup.sh
+# frontend/setup.sh
 
 echo "Setting up transit service"
 envsubst < backends/transit/.env.envsubst > backends/transit/.env
-backends/transit/setup.sh
+# backends/transit/setup.sh
 
 echo "Setting up calendar service"
 envsubst < backends/calendar/credentials.envsubst.json > backends/calendar/credentials.json
-backends/calendar/setup.sh
+# backends/calendar/setup.sh
 
 echo "Setting up kubernetes service"
 envsubst < backends/k8s-status-service/setup.envsubst.sh > backends/k8s-status-service/setup.sh
 chmod +x backends/k8s-status-service/setup.sh
-sudo backends/k8s-status-service/setup.sh
+# sudo backends/k8s-status-service/setup.sh
 
 echo "Setting up service units"
-mkdir -p ~/.config/systemd/user
-envsubst < share/amida-dashboard-frontend.service > ~/.config/systemd/user/amida-dashboard-frontend.service
-envsubst < share/amida-dashboard-transit.service > ~/.config/systemd/user/amida-dashboard-transit.service
-envsubst < share/amida-dashboard-calendar.service > ~/.config/systemd/user/amida-dashboard-calendar.service
-envsubst < share/amida-dashboard-k8s.service > ~/.config/systemd/user/amida-dashboard-k8s.service
-systemctl --user daemon-reload
-systemctl --user enable amida-dashboard-frontend
-systemctl --user start amida-dashboard-frontend
-systemctl --user enable amida-dashboard-transit
-systemctl --user start amida-dashboard-transit
-systemctl --user enable amida-dashboard-calendar
-systemctl --user start amida-dashboard-calendar
-systemctl --user enable amida-dashboard-k8s
-systemctl --user start amida-dashboard-k8s
+envsubst < share/amida-dashboard-frontend.service | sudo tee /etc/systemd/system/amida-dashboard-frontend.service
+envsubst < share/amida-dashboard-transit.service | sudo tee /etc/systemd/system/amida-dashboard-transit.service
+envsubst < share/amida-dashboard-calendar.service | sudo tee /etc/systemd/system/amida-dashboard-calendar.service
+envsubst < share/amida-dashboard-k8s.service | sudo tee /etc/systemd/system/amida-dashboard-k8s.service
+sudo systemctl daemon-reload
+sudo systemctl enable amida-dashboard-frontend
+sudo systemctl start amida-dashboard-frontend
+sudo systemctl enable amida-dashboard-transit
+sudo systemctl start amida-dashboard-transit
+sudo systemctl enable amida-dashboard-calendar
+sudo systemctl start amida-dashboard-calendar
+sudo systemctl enable amida-dashboard-k8s
+sudo systemctl start amida-dashboard-k8s
 
 echo "Setting up browser autostart"
-mkdir -p ~/.config/autostart
-cp share/dashboard.desktop ~/.config/autostart/dashboard.desktop
-# disable screensaver, setting DISPLAY env because gsettings has to talk to dbus
-sudo DISPLAY=:0 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+if [ "$KIOSK_MODE" = true ]; then
+	sudo mkdir -p $HOMEDIR/.config/autostart
+	sudo cp share/dashboard.desktop $HOMEDIR/.config/autostart/dashboard.desktop
+	# disable auto screen lock
+	sudo -u $USER HOME=/home/$USER dbus-launch --exit-with-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
 
-read -p "Press enter to restart lightdm into browser kiosk mode"
-sudo systemctl restart lightdm
+	read -p "Press enter to restart lightdm"
+	sudo systemctl restart lightdm
+fi
 
 popd
