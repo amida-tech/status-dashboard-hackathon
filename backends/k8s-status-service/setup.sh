9581befebef12e329c4cf867a6a41c1ccd5a0d2e
Harry Rickards <harry@amida.com>
2019-05-24 18:07:07 +0000

    Fix k8s status service start bug

--- backends/k8s-status-service/setup.sh
+++ backends/k8s-status-service/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 pushd "$(dirname "$0")"
 docker build -t k8s-status-service:0.0.1 .
-docker stop k8s-status-service && docker rm k8s-status-service
+docker stop k8s-status-service
+docker rm k8s-status-service
 docker create --name k8s-status-service -p 3001:3001 \
 -e K8S_STATUS_SERVICE_PORT=3001 \
 -v /home/odroid/dashboard/kubecfg:/root/.kube/config \
