47c40174356f437850b0ccf4438b25395fa5c61a
Harry Rickards <harry@amida.com>
2019-05-24 15:42:26 +0000

    Add password gen dep

--- preinstall.sh
+++ preinstall.sh
@@ -4,11 +4,26 @@ if [ ! -f /etc/lsb-release ]; then
 	echo "This script must be run on Ubuntu or another Debian-based system. Quitting..."
 	exit 1
 fi
+if [[ $EUID -eq 0 ]]; then
+	echo "This script should not be run as root. Quitting... Run again as your local user."
+	exit 1
+fi
 
 export USER=$(whoami)
 
+echo "Installing dependencies"
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs python3-venv python3-dev docker.io libpam0g-dev
+pushd /tmp
+rm -f dashboard-passwdqc.tar.gz
+wget https://www.openwall.com/passwdqc/passwdqc-1.3.1.tar.gz -O dashboard-passwdqc.tar.gz
+tar -xzf dashboard-passwdqc.tar.gz
+cd passwdqc-1.3.1
+make
+sudo make install
+popd
+
 echo "Configuring docker"
-sudo apt-get install docker.io
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo groupadd docker
