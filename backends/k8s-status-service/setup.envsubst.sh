94785c1e4c41e2dab5aae853e7ecb3f396613d38
Harry Rickards <harry@amida.com>
2019-05-24 17:17:21 +0000

    Get k8s sidebar working

--- backends/k8s-status-service/setup.envsubst.sh
+++ backends/k8s-status-service/setup.envsubst.sh
@@ -2,8 +2,8 @@
 pushd "$(dirname "$0")"
 docker build -t k8s-status-service:0.0.1 .
 docker rm k8s-status-service
-docker create --name k8s-status-service -p 6000:6000 \
--e K8S_STATUS_SERVICE_PORT=6000 \
+docker create --name k8s-status-service -p 3001:3001 \
+-e K8S_STATUS_SERVICE_PORT=3001 \
 -v ${BASEDIR}/kubecfg:/root/.kube/config \
 k8s-status-service:0.0.1
 popd
