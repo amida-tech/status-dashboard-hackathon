6697d979b3fad74c12b9e555aadc8d33134b0888
Unprofessional <unprofessionalmadman@gmail.com>
2019-06-01 22:04:10 -0400

    Make call to github oauth access_token work... but maybe not use this one? See note in commit diff

--- frontend/src/components/github-commit-thing/GitHubCommitDisplay.vue
+++ frontend/src/components/github-commit-thing/GitHubCommitDisplay.vue
@@ -29,6 +29,7 @@
 
 <script>
 import { mapActions, mapState } from 'vuex';
+import config from '../../config';
 
 import Vue from 'vue';
 
@@ -53,24 +54,52 @@ function getQueryStringValue (key) {
 }
 const code = getQueryStringValue('code');
 
+function queryParams(params) {
+    return Object.keys(params)
+        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+        .join('&');
+}
+
 if(code.length > 0) {
     // TODO: Make request for access_token
     const options = {
-        client_id: process.env.GH_CLIENT_ID, // not working
-        client_secret: process.env.GH_CLIENT_SECRET, // not working
-        code: code,
+        method: 'POST',
+        queryParams: {
+            client_id: config.ghClientId,//process.env.GH_CLIENT_ID, // not working
+            client_secret: config.ghClientSecret,//process.env.GH_CLIENT_SECRET, // not working
+            code: code,
+        },
         headers: {
-            "Accept": "application/json",
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
     };
-    console.log('>>>>> options: ', options);
+
+    console.log('>>>>> GitHubCommitDisplay > options[1]: ', options);
+
+    let url = 'https://github.com/login/oauth/access_token';
+
+    if(options.queryParams) {
+        url += (url.indexOf('?') === -1 ? '?' : '&') + queryParams(options.queryParams);
+        // delete options.queryParams;
+    }
+
+    // NOTE: Because... https://stackoverflow.com/a/42150336
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+
+    console.log('>>>>> GitHubCommitDisplay > options[2]: ', options);
+    console.log('>>>>> GitHubCommitDisplay > url: ', url);
 
     fetch(
-        'https://github.com/login/oauth/access_token',
-        options
+        proxyurl + url, // https://cors-anywhere.herokuapp.com/https://example.com
+        // 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token',
+        options,
     )
     .then((response) => {
-        console.log('GitHubCommitDisplay > response: ', respone);
+        console.log('>>>>> GitHubCommitDisplay > fetch oauth/access_token > response: ', response);
+    })
+    .catch((error) => {
+        console.log('>>>>> GitHubCommitDisplay > fetch oauth/access_token > error: ', error);
     });
 }
 
