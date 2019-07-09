e0480e06f0c2acc04843592f8e4da30b496dd391
Harry Rickards <harry@amida.com>
2019-07-09 08:58:15 -0400

    Add k8sUrl/calendarUrl vars to non-dockerized frontend config

--- frontend/src/config.envsubst.js
+++ frontend/src/config.envsubst.js
@@ -1,6 +1,8 @@
 const config = {
   wmataApiKey: '${WMATA_API_KEY}',
   transitUrl: 'http://localhost:5000',
+  k8sUrl: 'http://localhost:3001',
+  calendarUrl: 'http://localhost:3000',
 };
 
 export default config;
