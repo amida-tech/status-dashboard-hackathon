aa14de34864408db5c89c5ce76d7c6981ab3cc3e
Grant Orndorff <grant@amida.com>
2019-07-12 12:54:34 -0400

    adding /gcalwfh endpoint

--- backends/calendar/app.js
+++ backends/calendar/app.js
@@ -9,7 +9,20 @@ app.get('/gcal', wrapAsync(async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   let content = fs.readFileSync(process.cwd() + '/credentials.json');
   let auth = controller.authorize(JSON.parse(content));
-  eventList = await controller.listEvents(auth);
+  const gcalId = "amida-tech.com_9dugut48t480pb4qee57stskjs@group.calendar.google.com"
+  eventList = await controller.listEvents(auth, gcalId);
+  console.log('*** eventList ***');
+  console.log(eventList);
+  res.json(eventList);
+}));
+// Hackathon === Copypastathon
+app.get('/gcalwfh', wrapAsync(async function (req, res) {
+  // Load client secrets from a local file.
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  let content = fs.readFileSync(process.cwd() + '/credentials.json');
+  let auth = controller.authorize(JSON.parse(content));
+  const gcalId = "amida.com_jemhtrukfpict356ajn7lgv50g@group.calendar.google.com"
+  eventList = await controller.listEvents(auth, gcalId);
   console.log('*** eventList ***');
   console.log(eventList);
   res.json(eventList);
